ea1baa09426a1ee1a95b97bd67c969c3a72b3e29
TheRedWizard <sreeraghavan2016@gmail.com>
2025-08-19 23:54:32 +0530

    added camera analyser

---
+++ Heatmap/camera_position.py
@@ -0,0 +1,12 @@
+class CameraAnalysis:
+    def __init__(self):
+        self.camera_x = None
+        self.camera_y = None
+        self.camera_z = None
+
+        #table dimensions in cm
+        self.table_length = 274
+        self.table_width = 152.5
+        self.table_height = 76
+
+    #insert advanced math functions to magically solve everything here
