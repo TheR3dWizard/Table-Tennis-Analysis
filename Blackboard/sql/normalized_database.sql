26dbf2dad63da75094b23b76abfc2f3856a04be5
TheR3dWizard <sreeraghavan2016@gmail.com>
2025-10-22 17:26:02 +0530

    added normalized database

---
+++ Blackboard/sql/normalized_database.sql
@@ -0,0 +1,55 @@
+CREATE TABLE table_tennis_analysis (
+    videoId INTEGER,
+    frameId INTEGER,
+    frameAction VARCHAR,
+    tableid INTEGER,
+    ballvisibility BOOLEAN,
+    ballid INTEGER,
+    depthMapPath VARCHAR,
+    ballbounce BOOLEAN,
+    remarks VARCHAR,
+    PRIMARY KEY (videoId, frameId)
+);
+
+CREATE TABLE table_coords(
+    tableid INTEGER,
+    tablex1 FLOAT,
+    tabley1 FLOAT,
+    tablex2 FLOAT,
+    tabley2 FLOAT,
+    tablex3 FLOAT,
+    tabley3 FLOAT,
+    tablex4 FLOAT,
+    tabley4 FLOAT
+)
+
+CREATE TABLE ball_data(
+    ballId INTEGER,
+    ballx FLOAT,
+    bally FLOAT,
+    ballz FLOAT,
+    ballxvector FLOAT,
+    ballyvector FLOAT,
+    ballzvector FLOAT
+)
+
+CREATE TABLE player_positions(
+    playerid INTEGER,
+    playerx FLOAT,
+    playery FLOAT,
+    playerz FLOAT
+)
+
+CREATE TABLE bounces(
+    frameId INTEGER,
+    ballId INTEGER
+)
+
+CREATE TABLE video_table (
+    videoId INTEGER,
+    videoPath VARCHAR,
+    videoName VARCHAR,
+    videoTag VARCHAR,
+    fullvideoHeatmapPath VARCHAR,
+    videoDotMatrixSource VARCHAR
+);
