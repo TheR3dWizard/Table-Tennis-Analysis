3c828a29815c9df7f1eef81258a577ddcd5c8ea1
Akash Shanmugaraj <akashshanmugaraj@gmail.com>
2025-10-23 12:52:18 +0530

    Refactor consumer initialization and update logging method in computerandomplayercoordinates

--- Blackboard/InstantiateConsumers.py
+++ Blackboard/InstantiateConsumers.py
@@ -2,7 +2,7 @@ from TableVertexConsumer import TableVertexConsumer
 from BallPositionConsumer import Ball2DPositionConsumer
 from PlayerHeatmapConsumer import PlayerHeatmapConsumer
 from TrajectoryAnalysisConsumer import TrajectoryAnalysisConsumer
-from SpacePointSearch import SpacePointSearch
+# from SpacePointSearch import SpacePointSearch
 
 import threading
 import random
@@ -11,7 +11,7 @@ from constants import Constants
 
 
 def computerandomplayercoordinates():
-    print("Executing computerandomplayercoordinates....")
+    self.newprint("Executing computerandomplayercoordinates....")
     x, y, z = random.uniform(0, 1000), random.uniform(0, 1000), random.uniform(0, 1000)
 
     return (x, y, z)
@@ -24,26 +24,30 @@ def run_consumer(consumer):
 c1 = TableVertexConsumer(
     rabbitmqusername=Constants.RABBITMQ_USERNAME,
     rabbitmqpassword=Constants.RABBITMQ_PASSWORD,
+    id="table-vertex-detection-consumer",
 )
 c2 = Ball2DPositionConsumer(
     rabbitmqusername=Constants.RABBITMQ_USERNAME,
     rabbitmqpassword=Constants.RABBITMQ_PASSWORD,
+    id="ball-2d-position-detection",
 )
 c3 = PlayerHeatmapConsumer(
     rabbitmqusername=Constants.RABBITMQ_USERNAME,
     rabbitmqpassword=Constants.RABBITMQ_PASSWORD,
+    id="player-heatmap-consumer",
 )
 c4 = TrajectoryAnalysisConsumer(
     rabbitmqusername=Constants.RABBITMQ_USERNAME,
     rabbitmqpassword=Constants.RABBITMQ_PASSWORD,
+    id="trajectory-analysis",
 )
-c5 = SpacePointSearch(
-    rabbitmqusername=Constants.RABBITMQ_USERNAME,
-    rabbitmqpassword=Constants.RABBITMQ_PASSWORD,
-)
+# c5 = SpacePointSearch(
+#     rabbitmqusername=Constants.RABBITMQ_USERNAME,
+#     rabbitmqpassword=Constants.RABBITMQ_PASSWORD,
+# )
 
 threads = []
-for consumer in [c1, c2, c3, c4, c5]:
+for consumer in [c1, c2, c3, c4]:  # , c5
     t = threading.Thread(target=run_consumer, args=(consumer,))
     t.start()
     threads.append(t)
